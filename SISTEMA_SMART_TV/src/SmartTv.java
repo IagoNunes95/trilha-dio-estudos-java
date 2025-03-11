1f60f8627fa0dd1d7e25a9f48ee637bb84b76684
Iago Nunes <iagonunesfrancino@hotmail.com>
2025-03-11 12:53:24 -0300

    atualização metodos smartTv

--- SISTEMA_SMART_TV/src/SmartTv.java
+++ SISTEMA_SMART_TV/src/SmartTv.java
@@ -1,4 +1,35 @@
 
 public class SmartTv {
-
+	boolean ligada=false;
+	int canal=1;
+	int volume = 25;
+	
+	public void mudarCanal(int novoCanal) {
+		canal = novoCanal;
+		System.out.println("Canal alterado para "+canal);
+	}
+	
+	public void aumentarCanal() {
+		canal++;
+	}
+	public void diminuirCanal() {
+		canal--;
+	}
+	
+	public void aumentarVolume() {
+		volume++;
+		System.out.println("aumentando o volume para "+volume);
+	}
+	
+	public void diminuirVolume() {
+		volume--;
+		System.out.println("diminuindo o volume para "+volume);
+	}
+	
+	public void ligar() {
+		ligada=true;
+	}
+	public void desligar() {
+		ligada=false;
+	}
 }
